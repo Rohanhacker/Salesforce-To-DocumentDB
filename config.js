450d9b4b5f4d594abd8d88b3f69202d7492e4b49
Rohan <rohan@localhost.localdomain>
2016-09-12 08:32:53 +0530

    working

---
+++ config.js
@@ -0,0 +1,15 @@
+let config = {}
+
+// You should have collections created in
+// documentdb with names which are similar
+// to your salesforce table names
+
+config.endpoint = "Document DB end point";
+config.database = "Your documentdb database name";
+config.primarykey = "primary key docdb";
+config.tables = []; // enter names of sf tables
+config.sfUsername = "salesforce username";
+config.sfPassword = "Your Password";
+config.sfSecurityToken = "salesforce security token";
+
+module.exports = config;
---
+++ start.js
@@ -0,0 +1,51 @@
+const config = require('./config');
+const jsforce = require('jsforce');
+var documentClient = require("documentdb").DocumentClient;
+const conn = new jsforce.Connection();
+const url=require("url");
+const client = new documentClient(config.endpoint, { "masterKey": config.primarykey });
+const databaseUrl = `dbs/${config.database}`;
+
+let tables = config.tables;
+
+conn.login(config.sfUsername, config.sfPassword+config.sfSecurityToken, (err, res) => {
+  if (err) { return console.error(err); }
+  tables.map(schema);
+});
+
+function schema(table_name) {
+  return (conn.sobject(table_name).describe((err, meta) => {
+    if (err) { return console.error(err); }
+    let len=meta.fields.length;
+    let records = [];
+    let str = "";
+    let A = [];
+    for(let i=0;i<len;i++) {
+      if(meta.fields[i].name!="ID") {
+        records[i] = meta.fields[i].name;
+      }
+    }
+    str = records.join();
+    conn.query("SELECT " + str + " from "+table_name)
+    .on('record', function(rec) {
+      delete rec.attributes
+      A.push(rec);
+      })
+    .on('error', function(err) {
+      console.log(err);
+    }).on('end',function() {
+      let collection_name = "test"; // this must be table_name and table_name = collection.id
+      let collectionUrl = `${databaseUrl}/colls/${collection_name}`;
+      for(let i=0;i<A.length;i++) {
+        client.createDocument(collectionUrl, A[i], (err, created) => {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log("created");
+          }
+        });
+      }
+    })
+  }));
+}
